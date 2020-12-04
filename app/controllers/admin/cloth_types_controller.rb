666d3d8b46f8082611d826d20e5ae6e1650a2e26
Manuel Topete <manueltopt96@gmail.com>
2020-12-04 00:31:38 -0600

    ROLLBACK last 3997a57eb and ebeb31106 break

--- app/controllers/admin/cloth_types_controller.rb
+++ app/controllers/admin/cloth_types_controller.rb
@@ -1,7 +1,5 @@
 module Admin
   class ClothTypesController < ApplicationController
     include ClothTypesConcern
-
-    before_action :authorized_user
   end
 end
--- app/controllers/admin/cloths_controller.rb
+++ app/controllers/admin/cloths_controller.rb
@@ -1,7 +1,5 @@
 module Admin
   class ClothsController < ApplicationController
     include ClothsConcern
-
-    before_action :authorized_user
   end
 end
