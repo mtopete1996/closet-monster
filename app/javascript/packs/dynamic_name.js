674bec73d86923239ed4f79ace198c27c875da8a
Manuel Topete <manueltopt96@gmail.com>
2021-03-07 21:46:27 -0600

    Revert "Generate dynamic name if name input is empty"
    This reverts commit c2515c78d2fc6bc491687f4b489c4d784295f275.

--- app/javascript/packs/dynamic_name.js
+++ app/javascript/packs/dynamic_name.js
@@ -5,12 +5,14 @@ document.addEventListener("turbolinks:load", function() {
     const $nameField = $form.find('input[name="cloth[name]"]')
     const $typeField = $form.find('select[name="cloth[cloth_type_id]"]')
     const $brandField = $form.find('select[name="cloth[cloth_brand_id]"]')
+    let has_been_changed = false
 
-    $typeField.on('change', () => updateName())
-    $brandField.on('change', () => updateName())
+    $typeField.change(() => updateName())
+    $brandField.change(() => updateName())
+    $nameField.change(() => { has_been_changed = true })
 
     const updateName = () => {
-      if ($nameField.val() === '') {
+      if (!has_been_changed) {
         const selectedType = $typeField.find('option:selected').text()
         const selectedBrand = $brandField.find('option:selected').text()
         const newName = [selectedType, selectedBrand].join(' ')
